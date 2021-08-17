6ce22b20fd04b75b3a691ce45c31b506739f9370
nastassia-mav <88528356+nastassia-mav@users.noreply.github.com>
2021-08-17 12:07:58 +0300

    UPD

--- lesson7.php
+++ lesson7.php
@@ -1,13 +1,13 @@
 <?php
-//EXERCIZE 1
+//EXERCISE 1
 $str = 'php';
 echo strtoupper($str);
 
-//EXERCIZE 6
+//EXERCISE 6
 $str = 'LONDON';
 echo  ucfirst(strtolower($str));
 
-//EXERCIZE 8
+//EXERCISE 8
 $password = 'LONDON';
 if(strlen($password) < 10 and strlen($password) > 5){
     echo 'Пароль подходит';
@@ -15,7 +15,7 @@ if(strlen($password) < 10 and strlen($password) > 5){
     echo 'Пароль не подходит';
 }
 
-//EXERCIZE 15
+//EXERCISE 15
 $password = 'LONDON';
 if(strlen($password) > 5){
     echo substr($password, 1, 5).'...';
