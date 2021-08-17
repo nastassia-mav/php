2796d76cc5b994637d9c709be6cd6d52e6053c9b
nastassia-mav <88528356+nastassia-mav@users.noreply.github.com>
2021-08-17 12:06:58 +0300

    UPD

--- lesson5.php
+++ lesson5.php
@@ -1,12 +1,12 @@
 <?php
 
-//EXERCIZE 1
+//EXERCISE 1
 $arr = ['html', 'css', 'php', 'js', 'jq'];
 foreach($arr as $elem){
     echo $elem.'<br>';
 }
 
-//EXERCIZE 2
+//EXERCISE 2
 $arr = [1, 2, 3, 4, 5];
 $result = 0;
 foreach($arr as $elem){
@@ -14,7 +14,7 @@ foreach($arr as $elem){
 }
 echo $result;
 
-//EXERCIZE 3
+//EXERCISE 3
 $arr = [1, 2, 3, 4, 5];
 $result = 0;
 foreach($arr as $elem){
@@ -22,19 +22,19 @@ foreach($arr as $elem){
 }
 echo $result;
 
-//EXERCIZE 4
+//EXERCISE 4
 $arr = ['green'=>'зеленый', 'red'=>'красный','blue'=>'голубой'];
 foreach($arr as $key=>$elem){
     echo $key.'-'.$elem.'<br>';
 }
 
-//EXERCIZE 5
+//EXERCISE 5
 $arr = ['Коля'=>'200', 'Вася'=>'300','Петя'=>'400'];
 foreach($arr as $key=>$elem){
     echo $key.' - зарплата '.$elem.' долларов. <br>';
 }
 
-//EXERCIZE 6
+//EXERCISE 6
 $res = 1;
 while($res <= 100){
     echo $res.'<br>';
@@ -45,7 +45,7 @@ for ($res=1; $res<=100; $res++){
     echo $res.'<br>';
 }
 
-//EXERCIZE 7
+//EXERCISE 7
 $res = 11;
 while($res <= 33){
     echo $res.'<br>';
@@ -56,7 +56,7 @@ for ($res=11; $res<=33; $res++){
     echo $res.'<br>';
 }
 
-//EXERCIZE 8
+//EXERCISE 8
 $res = 11;
 while($res <= 33){
     echo $res.'<br>';
@@ -67,7 +67,7 @@ for ($res=11; $res<=33; $res++){
     echo $res.'<br>';
 }
 
-//EXERCIZE 10
+//EXERCISE 10
 $res = [2, 5, 9, 15, 0, 4];
 foreach($res as $items){
     if($items >= 3 and $items <= 10){
@@ -75,7 +75,7 @@ foreach($res as $items){
     }
 }
 
-//EXERCIZE 13
+//EXERCISE 13
 $arr = ['10', '20', '30', '50', '235', '3000'];
 foreach ($arr as $items){
     if($items[0] == '1' or $items[0] == '2' or $items[0] == '5'){
@@ -83,14 +83,14 @@ foreach ($arr as $items){
     }
 }
 
-//EXERCIZE 17
+//EXERCISE 17
 $arr = [];
 for ($i=1; $i<=100; $i++){
     array_push($arr, $i);
 }
 var_dump($arr);
 
-//EXERCIZE 19
+//EXERCISE 19
 $num = 1000;
 $total = 0;
 while($num >= 50){
