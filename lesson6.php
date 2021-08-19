dca3e3ad4a2e266b165f6f31d2ebf780d71999b9
nastassia-mav <88528356+nastassia-mav@users.noreply.github.com>
2021-08-19 22:03:39 +0300

    Update lesson 6

--- lesson6.php
+++ lesson6.php
@@ -1,14 +1,14 @@
 <?php
 //EXERCISE 1
-$a=10;
-$b=3;
+$a = 10;
+$b = 3;
 echo $a % $b;
 
 //EXERCISE 5
 $total = 0;
 $arr = [4, 2, 5, 19, 13, 0, 10];
-foreach($arr as $i){
-    $total += $i*$i;
+foreach ($arr as $i) {
+    $total += $i * $i;
 }
 echo sqrt($total);
 
@@ -21,12 +21,12 @@ var_dump($arr);
 
 //EXERCISE 8
 $arr = [4, -2, 5, 19, -130, 0, 10];
-echo max($arr).' '.min($arr);
+echo max($arr) . ' ' . min($arr);
 
 //EXERCISE 10
 $arr = [];
 $num = 10;
-while($num > 0){
+while ($num > 0) {
     $num--;
     array_push($arr, mt_rand());
 }
@@ -34,8 +34,8 @@ var_dump($arr);
 
 //EXERCISE 12
 $arr = [1, 2, -1, -2, 3, -3];
-for($i=0; $i < 7; $i++){
-    if($arr[$i] < 0){
+for ($i = 0; $i < 7; $i++) {
+    if($arr[$i] < 0) {
         $arr[$i] = abs($i);
     }
 }
@@ -45,8 +45,8 @@ var_dump($arr);
 $arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 $total = 0;
 $sum = 1;
-while($total < 10){
-    foreach($arr as $i){
+while ($total < 10) {
+    foreach ($arr as $i) {
         $sum ++;
         $total += $i;
     }
