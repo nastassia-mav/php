338938d83d27e59fcd55d2293ab617c3dd614d48
nastassia-mav <88528356+nastassia-mav@users.noreply.github.com>
2021-08-19 14:15:52 +0300

    Lesson 14

---
+++ lesson14.php
@@ -0,0 +1,26 @@
+<?php
+
+//EXERCISE 1
+function func($a, $b) 
+{
+    return $a == $b;
+}
+echo func(5, 5);
+
+
+//EXERCISE 2
+function func1($a, $b) 
+{
+    return $a + $b > 10;
+}
+echo func1(6, 5);
+
+
+
+
+//EXERCISE 3
+function func2($a) 
+{
+    return $a > 0;
+}
+echo func2(6);
