f0e61dfcd548d50cda95b15771260e96f1688bb6
nastassia-mav <88528356+nastassia-mav@users.noreply.github.com>
2021-08-20 10:09:18 +0300

    Update lesson 13

--- lesson13.php
+++ lesson13.php
@@ -8,10 +8,9 @@ foreach ($arr as $value) {
 		break;
 	}
 }
-
 if ($flag) {
 	echo "есть";
-}else{
+} else {
 	echo "нет";
 }
 
@@ -19,38 +18,31 @@ if ($flag) {
 //EXERCISE 2
 $flag = false;
 $int = 31;
-
 for ($i=2; $i <= 30; $i++) {
 	if (!($int % $i)) {
 		$flag = true;
 	}
 }
-
 if ($flag) {
 	echo "есть";
-}else{
+} else {
 	echo "нет";
 }
 
 
 //EXERCISE 3
-
 $flag = false;
 $int = 0;
-
 $arr = [1, 2, 3, 4, 5, 6, 7, 7, 8];
-
 foreach ($arr as $value) {
-	if($int == $value){
+	if ($int == $value) {
 		$flag = true;
-	}else{
+	} else {
 		$int = $value;
 	}
 }
-
-
 if ($flag) {
 	echo "есть";
-}else{
+} else {
 	echo "нет";
 }	
