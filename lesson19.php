b8a7972b3c9ab8499159a6adafa5f37a179bd751
nastassia-mav <88528356+nastassia-mav@users.noreply.github.com>
2021-08-20 10:26:04 +0300

    Update lesson 19

--- lesson19.php
+++ lesson19.php
@@ -1,4 +1,23 @@
 <?php
+
+//EXERCISE 1
+function cut($string, $how_first_symbols = '10'){
+$string = mb_substr($string, 0, $how_first_symbols, 'UTF-8');
+return $string;
+}
+echo cut('Длинная строка', 5);
+
+//EXERCISE 2
+$arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+function show_array($arr){
+$elem = array_shift($arr);
+echo $elem;
+if (!empty($arr)) {
+    show_array($arr);
+    }
+}
+show_array($arr); 
+
 //EXERCISE 3
 function lowNum($num)
 {
@@ -9,26 +28,3 @@ function lowNum($num)
     return $result;
 }
 
-//EXERCISE 1
-function cut($string, $how_first_symbols = '10'){
-$string = mb_substr($string, 0, $how_first_symbols, 'UTF-8');
-return $string;
-}
-
-echo cut('Длинная строка', 5);
-
-//EXERCISE 2
-
-$arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-function show_array($arr){
-
-$elem = array_shift($arr);
-echo $elem;
-
-if(!empty($arr)) {
-    show_array($arr);
-}
-}
-
-show_array($arr); // Выведет 123456789
