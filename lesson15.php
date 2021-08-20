6ee7e3ac04782534bebe9e9d5ef1008738586ae5
nastassia-mav <88528356+nastassia-mav@users.noreply.github.com>
2021-08-20 10:12:29 +0300

    Update lesson 15

--- lesson15.php
+++ lesson15.php
@@ -1,25 +1,25 @@
 <?php
+
 //EXERCISE 1
 $str = '';
-
-for ($i=1; $i <= 9; $i++) { 
+for ($i = 1; $i <= 9; $i++) { 
 	$str .= $i;
 }
-echo $str."<br>";
+echo $str . '<br>';
 
 //EXERCISE 2
 $str = '';
-for ($i=9; $i > 0; $i--) { 
+for ($i = 9; $i > 0; $i--) { 
 	$str .= $i;
 }
-echo $str."<br>";
+echo $str . '<br>';
 
 //EXERCISE 3
 $str = '-';
-for ($i=1; $i < 10; $i++) { 
+for ($i = 1; $i < 10; $i++) { 
 	$str .= $i.'-';
 }
-echo $str."<br>";
+echo $str . '<br>';
 
 //EXERCISE 4
 $str = '';
@@ -28,29 +28,25 @@ for ($i = 1; $i <= 20; $i++) {
     for ($j = 1; $j <= $i; $j++) {
         $str .= 'x';
     }
-    echo $str.'<br>';
+    echo $str . '<br>';
 }
 
-
-
-
 //EXERCISE 5
-
 $str = '';
 for ($i = 1; $i <= 9; $i++) {
 	 $str = '';
     for ($j = 1; $j <= $i; $j++) {
         $str .= $i;
     }
-    echo $str.'<br>';
+    echo $str . '<br>';
 }
 
 //EXERCISE 6
 $str = '';
-for ($i=1; $i <= 5; $i++) { 
+for ($i = 1; $i <= 5; $i++) { 
 	$str = '';
-	for ($j=0; $j < $i; $j++) { 
+	for ($j = 0; $j < $i; $j++) { 
 		$str .= 'xx';
 	}
-	echo $str.'<br>';
+	echo $str . '<br>';
 }
