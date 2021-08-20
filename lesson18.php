88274f24bdfe0262b98f958ffdb238c60e6b68c8
nastassia-mav <88528356+nastassia-mav@users.noreply.github.com>
2021-08-20 10:24:17 +0300

    Update lesson 18

--- lesson18.php
+++ lesson18.php
@@ -29,66 +29,53 @@ function translit($string){
 );
 return strtr($string, $converter);
 }
-
 echo translit('Котики');
-
-echo "<br>";
+echo '<br>';
 
 //EXERCISE 2
 function getPluralOrSingular($number, $form_one, $form_two_three_four, $form_many){
 if ($number == 1) {
-    $result = $number.' '.$form_one;
+    $result = $number . ' ' . $form_one;
 }
-if ($number >1 and $number < 5) {
-    $result = $number.' '.$form_two_three_four;
+if ($number > 1 and $number < 5) {
+    $result = $number . ' ' . $form_two_three_four;
 }
 if ($number > 4) {
-    $result = $number.' '.$form_many;
+    $result = $number . ' ' . $form_many;
 }
 return $result;
 }
-
 echo getPluralOrSingular(6, 'банан', 'банана', 'бананов'); 
 
-
-
 //EXERCISE 3
-
 function happyTicket($string){
 $arr = str_split($string, 1);
-
 $sum_first3_dig = 0;
-for ($i=0; $i < 3; $i++) { 
+for ($i = 0; $i < 3; $i++) { 
     $sum_first3_dig += $arr[$i];
 }
-
 $sum_second3_dig = 0;
-for ($g=3; $g < 6; $g++) { 
+for ($g = 3; $g < 6; $g++) { 
     $sum_second3_dig += $arr[$g];
 }
-
 return $sum_first3_dig+$sum_second3_dig;
 }
-
-echo happyTicket(555122); 
-
-echo "<br>";
+echo happyTicket(5552); 
+echo '<br>';
 
 //EXERCISE 4
 function getDivisorsSum($number){
 $sum = 0;
-for ($i=1; $i < $number; $i++) { 
+for ($i = 1; $i < $number; $i++) { 
     if ($number % $i === 0) {
         $sum += $i;
     }
 }
 return $sum;
 }
-
-
 function frendlyNumber($n_one, $n_two)
 {
 if (getDivisorsSum($n_one) == $n_two and getDivisorsSum($n_two) == $n_one and $n_one !== $n_two and $n_one < $n_two) {
-    return "Числа ".$n_one.' и '.$n_two.' являются дружественными.<br>';
+    return 'Числа ' . $n_one.' и ' . $n_two . ' являются дружественными. <br>';
 }
 }
