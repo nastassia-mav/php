5345c77d85c6a0ec8dd85cb5af68e7c3324f5fa5
nastassia-mav <88528356+nastassia-mav@users.noreply.github.com>
2021-09-15 23:29:49 +0300

    Update lesson 20

--- lesson20.php
+++ lesson20.php
@@ -1,15 +1,15 @@
 //EXERCISE 1
-<form action = "" method = "post">
+<form action="" method="post">
     <fieldset>
-        <input type = "text" name = "name" placeholder = "Введите Имя" required>
-        <input type = "hidden" name = "hi" value = "0">
-        <input type = "checkbox" name = "hi" value = "1"> 
-        <input type = "submit"><br><br>
+        <input type="text" name="name" placeholder="Введите Имя" required>
+        <input type="hidden" name="hi" value="0">
+        <input type="checkbox" name="hi" value="1">
+        <input type="submit"><br><br>
         <?php
-        if (isset($_REQUEST["hi"]) and $_REQUEST["hi"] == 1) {
+        if (isset($_REQUEST["hi"]) && $_REQUEST["hi"] == 1) {
             echo "Приветствую, " . $_REQUEST["name"];
         }
-        if (isset($_REQUEST["hi"]) and $_REQUEST["hi"] == 0) {
+        if (isset($_REQUEST["hi"]) && $_REQUEST["hi"] == 0) {
             echo "До новых встреч, " . $_REQUEST["name"];
         }
         ?>
@@ -17,14 +17,14 @@
 </form>
 
 //EXERCISE 2
-<form action = "" method = "post">
+<form action="" method="post">
     <fieldset>
         Укажите технологии, которыми вы владеете:<br>
-        <input type = "checkbox" name = "lang[]" value = "php" checked>&nbsp;php<br>
-        <input type = "checkbox" name = "lang[]" value = "html">&nbsp;html<br>
-        <input type = "checkbox" name = "lang[]" value = "css">&nbsp;css<br>
-        <input type = "checkbox" name = "lang[]" value = "javascript">&nbsp;javascript<br><br>
-        <input type = "submit" name = "submit"><br><br>
+        <input type="checkbox" name="lang[]" value="php" checked>&nbsp;php<br>
+        <input type="checkbox" name="lang[]" value="html">&nbsp;html<br>
+        <input type="checkbox" name="lang[]" value="css">&nbsp;css<br>
+        <input type="checkbox" name="lang[]" value="javascript">&nbsp;javascript<br><br>
+        <input type="submit" name="submit"><br><br>
         <?php
         if (!empty($_REQUEST['submit'])) {
             if (isset($_REQUEST['lang'])) {
@@ -38,12 +38,12 @@
 </form>
 
 //EXERCISE 3
-<form action = "" method = "post">
+<form action="" method="post">
     <fieldset>
         Вы знаете PHP?<br>
-        <input type = "radio" name = "test" value="1" checked> - Y <br>
-        <input type = "radio" name = "test" value="2"> - N <br>
-        <input type = "submit" name = "submit_2"><br><br>
+        <input type="radio" name="test" value="1" checked> - Y <br>
+        <input type="radio" name="test" value="2"> - N <br>
+        <input type="submit" name="submit_2"><br><br>
         <?php
         if (!empty($_REQUEST["submit_2"])) {
             if ($_REQUEST["test"] == 1) echo "Да";
@@ -54,14 +54,14 @@
 </form>
 
 //EXERCISE 4
-<form action = "" method = "post">
+<form action="" method="post">
     <fieldset>
         Укажите возраст:<br>
-        <input type = "radio" name = "test" value = "1"> - менее 20 лет <br>
-        <input type = "radio" name = "test" value = "2"> - 20-25 <br>
-        <input type = "radio" name = "test" value = "3"> - 26-30 <br>
-        <input type = "radio" name = "test" value = "4"> - более 30 <br>
-        <input type = "submit"><br><br>
+        <input type="radio" name="test" value="1"> - менее 20 лет <br>
+        <input type="radio" name="test" value="2"> - 20-25 <br>
+        <input type="radio" name="test" value="3"> - 26-30 <br>
+        <input type="radio" name="test" value="4"> - более 30 <br>
+        <input type="submit"><br><br>
         <?php
         if (!empty($_REQUEST["test"])) {
             echo "Вы указали: ";
@@ -75,30 +75,30 @@
 </form>
 
 //EXERCISE 5
-<form action = "" method = "post">
+<form action="" method="post">
     <fieldset>
-        Укажите возраст:&nbsp;
+        Укажите возраст:
         <select>
             <option>менее 20 лет</option>
             <option>20-25</option>
             <option>26-30</option>
             <option>более 30 лет</option>
-        </select>&nbsp;
-        <input type = "submit"><br><br>
+        </select>
+        <input type="submit"><br><br>
     </fieldset>
 </form>
 
 //EXERCISE 6
-<form action = "" method = "post">
+<form action="" method="post">
     <fieldset>
         <p>Какие языки вы знаете? (зажать клавишу Ctrl)</p>
-        <select multiple name = "lang[]">
-            <option value = "php">php</option>
-            <option value = "css">css</option>
-            <option value = "html">html</option>
-            <option value = "javascript">javascript</option>
+        <select multiple name="lang[]">
+            <option value="php">php</option>
+            <option value="css">css</option>
+            <option value="html">html</option>
+            <option value="javascript">javascript</option>
         </select>&nbsp;
-        <input type = "submit"><br><br>
+        <input type="submit"><br><br>
         <?php
         if (!empty($_REQUEST["lang"])) {
             echo "Вы указали: " . implode(", ", $_REQUEST["lang"]) . ".";
@@ -108,7 +108,7 @@
 </form>
 
 //EXERCISE 7-8
-<form action = "" method = "post">
+<form action="" method="post">
     <fieldset>
         <?php
         function input($type, $name, $value)
@@ -119,14 +119,13 @@
             return '<input type="' . $type . '" name="' . $name . '" value="' . $value . '" 
 placeholder="Введите сообщение"><input type="submit">';
         }
-
         echo input("text", "User", 19);
         ?>
     </fieldset>
 </form>
 
 //EXERCISE 9
-<form action = "" method = "post">
+<form action="" method="post">
     <fieldset>
         <?php
         echo checkbox("check");
@@ -138,13 +137,13 @@ placeholder="Введите сообщение"><input type="submit">';
 <?php
 function checkbox($name)
 {
-    return '<input type="hidden" name="' . $name . '" value="0">
-            <input type="checkbox" name="' . $name . '" value="1">';
+    $format = '<input type="hidden" name=%s value="0"><input type="checkbox" name=%s value="1">';
+    return sprintf($format, $name, $name);
 }
 ?>
 
 //EXERCISE 10
-<form action = "" method = "post">
+<form action="" method="post">
     <fieldset>
         <?php
         echo check("check1");
@@ -153,14 +152,14 @@ function checkbox($name)
         echo check("check4");
         echo check("check5");
         ?>
-        <input type = "submit">
+        <input type="submit">
     </fieldset>
 </form>
 
 <?php
 function check($name)
 {
-    if (!empty($_REQUEST[$name])) $checked = "checked"; else $checked = "";
+    $checked = !empty($_REQUEST[$name]) ? "checked" : "";
     return '<input type="hidden" name="' . $name . '" value="0">
 <input type="checkbox" name="' . $name . '" value="1" ' . $checked . '>';
 }
