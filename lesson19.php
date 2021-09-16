17b68ff364c4c3a697b40f6c942c7c18793915ab
nastassia-mav <88528356+nastassia-mav@users.noreply.github.com>
2021-09-16 11:19:55 +0300

    Added "return type"

--- lesson19.php
+++ lesson19.php
@@ -1,19 +1,18 @@
 <?php
 
 //EXERCISE 1
-function cut($string, $howFirstSymbols = '10')
+function cut(string $string, string $howFirstSymbols = '10'): string
 {
-    $string = mbSubstr($string, 0, $howFirstSymbols, 'UTF-8');
+    $string = mb_substr($string, 0, $howFirstSymbols, 'UTF-8');
     return $string;
 }
-
 echo cut('Длинная строка', 5);
 
 //EXERCISE 2
 $arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-function showArray($arr)
+function showArray(array $arr): array
 {
-    $elem = arrayShift($arr);
+    $elem = array_shift($arr);
     echo $elem;
     if (!empty($arr)) {
         showArray($arr);
@@ -22,9 +21,9 @@ function showArray($arr)
 showArray($arr);
 
 //EXERCISE 3
-function lowNum($arr)
+function lowNum(array $arr): int
 {
-    $result = arraySum($arr);
+    $result = array_sum($arr);
     if ($result > 9) {
         $result = lowNum($result);
     }
